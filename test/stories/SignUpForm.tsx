8c4f02af8bcf968d485cefc22f205d351f3821a5
Taehee Kim <rabolution@gmail.com>
2025-12-18 21:41:47 +0900

    refactor: SimpleTextInput, SimpleErrorMessage 추출

--- test/stories/SignUpForm.tsx
+++ test/stories/SignUpForm.tsx
@@ -21,8 +21,6 @@ export function SignUpForm({
 		email: undefined,
 		password: undefined,
 	} as { email: string | undefined; password: string | undefined });
-	const inputId = useId();
-	const errorId = useId();
 	return (
 		<form
 			onSubmit={(event) => {
@@ -47,50 +45,65 @@ export function SignUpForm({
 				);
 			}}
 		>
-			<label>
-				이메일
-				<input
-					type="email"
-					name="email"
-					aria-invalid={error.email ? "true" : undefined}
-					aria-describedby={error.email ? "error-email" : ""}
-					aria-errormessage={error.email ? "error-email" : ""}
-				/>
-			</label>
-			{error.email && (
-				<div
-					id={"error-email"}
-					role="alert"
-					aria-live="assertive"
-					aria-label={error.email}
-				>
-					{error.email}
-				</div>
-			)}
-
-			<label htmlFor={inputId}>비밀번호</label>
-			<input
-				id={inputId}
-				// biome-ignore lint/a11y/noRedundantRoles: password input is not textbox role default
-				role="textbox"
-				type="password"
-				name="password"
-				aria-invalid={error.password ? "true" : undefined}
-				aria-describedby={error.password ? errorId : ""}
-				aria-errormessage={error.password ? errorId : ""}
+			
+			<SimpleTextInput
+				name="email"
+				type="email"
+				label="이메일"
+				error={error}
 			/>
-			{error.password && (
-				<div
-					id={errorId}
-					role="alert"
-					aria-live="assertive"
-					aria-label={error.password}
-				>
-					{error.password}
-				</div>
-			)}
+
+			<SimpleTextInput
+				name="password"
+				type="password"
+				label="비밀번호"
+				error={error}
+			/>
 
 			<button type="submit">가입하기</button>
 		</form>
 	);
 }
+
+function SimpleTextInput({ name, type, label, error }: {
+	name: string,
+	type: "text" | "email" | "password",
+	label: string,
+	error: Record<string, string | undefined>,
+}) {
+	const inputId = useId();
+	const errorId = useId();
+	return (
+		<>
+			<label htmlFor={inputId}>{label}</label>
+			<input
+				id={inputId}
+				role="textbox"
+				type={type}
+				name={name}
+				aria-invalid={error[name] ? "true" : undefined}
+				aria-describedby={error[name] ? errorId : ""}
+				aria-errormessage={error[name] ? errorId : ""}
+			/>
+			<SimpleErrorMessage error={error} name={name} errorId={errorId} />
+		</>
+	)
+}
+
+function SimpleErrorMessage({ error, name, errorId }: {
+	name: string,
+	error: Record<string, string | undefined>,
+	errorId: string
+}) {
+	return error[name] && (
+		<div
+			id={errorId}
+			role="alert"
+			aria-live="assertive"
+			aria-label={error[name]}
+		>
+			{error[name]}
+		</div>
+	);
+}
+
